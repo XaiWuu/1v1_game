88789bf350f5f36e58fbf6919a3bcb477f51b174
Gabriel Larsson <gabbelarsson777@gmail.com>
2021-10-19 10:37:09 +0200

    uugcoc

--- 1v1_game/Program.cs
+++ 1v1_game/Program.cs
@@ -1,42 +1,71 @@
 ﻿using System;
 using System.Threading;
 {
-     
+
 }
 
-int P1hp = 100;
-int P2hp = 100;
+int P1hp = 350;
+int P2hp = 350;
+string name1;
+string name2;
 
+name1 = "";
+name2 = "";
 
 Random generator = new Random();
 
-Console.WriteLine($"Player 1 have {P1hp}hp");
-Thread.Sleep(1000);
-Console.WriteLine($"PLayer 2 have {P2hp}hp");
-Thread.Sleep(1000);
-int r = generator.Next(2);
 
+Console.WriteLine($"Player 1 write your name");
+name1 = Console.ReadLine();
+
+Console.WriteLine($"Player 2 write your name");
+name2 = Console.ReadLine();
+Console.WriteLine();
+
+Thread.Sleep(1000);
+Console.WriteLine($"{name1} has {P1hp}hp");
+Thread.Sleep(1000);
+Console.WriteLine($"{name2} has {P2hp}hp");
+Thread.Sleep(1000);
+Console.WriteLine();
+
+int r = generator.Next(2);
 
 if (r == 0)
 {
-    Console.WriteLine($"Player 1 start");
+    Console.WriteLine($"{name1} start");
 
-while (P1hp >= 0 && P2hp >= 0)
-{
-    Stuff.fight1();
-    Stuff.fight2();
-}
+    while (P1hp >= 0 && P2hp >= 0)
+    {
+        (int p1, int p2) result1 = Stuff.fight1(P2hp, P1hp, name1, name2);
+        P2hp = result1.p1;
+        P1hp = result1.p2;
+        
+        (int p2, int p1) result2 = Stuff.fight2(P1hp, P2hp, name1, name2);
+        P1hp = result2.p2;
+        P2hp = result2.p1;
+    }
+
 }
 
 if (r == 1)
 {
-    Console.WriteLine($"Player 2 start");
+    Console.WriteLine($"{name2} start");
 
     while (P1hp >= 0 && P2hp >= 0)
-{
-    Stuff.fight2();
-    Stuff.fight1();
-}
+    {
+        (int p2, int p1) result2 = Stuff.fight2(P1hp, P2hp, name1, name2);
+        P1hp = result2.p2;
+        P2hp = result2.p1;
+
+         (int p1, int p2) result1 = Stuff.fight1(P2hp, P1hp, name1, name2);
+        P2hp = result1.p1;
+        P1hp = result1.p2;
+
+    }
+
+
 }
 
 Console.ReadLine();
+
--- 1v1_game/idk.cs
+++ 1v1_game/idk.cs
@@ -2,62 +2,213 @@ using System;
 using System.Threading;
 public class Stuff
 {
-    public static void fight1()
-    {   
-        int P1hp = 100;
-        int P2hp = 100;
-
-        Random generator = new Random();
-        int x = generator.Next(5, 11);
-        int y = generator.Next(10, 21);
-
-        string answer = "";
-        answer = answer.ToLower();
-        while (answer != "punch" && answer != "kick")
-        {
-            Thread.Sleep(1000);
-            Console.WriteLine($"Player 1 punch or kick?");
-            answer = Console.ReadLine();
-            
-        }
-        if (answer == "punch")
-        {
-            P2hp = P2hp - x;
-            Console.WriteLine($"Player 2 has {P2hp}hp left");
-        }
-        if (answer == "kick")
-        {
-            P2hp = P2hp - y;
-            Console.WriteLine($"Player 2 has {P2hp}hp left");
-        }
-    }
-    public static void fight2()
+    public static (int, int) fight1(int player1, int player2, string name1, string name2)
     {
-        int P1hp = 100;
-        int P2hp = 100;
 
         Random generator = new Random();
-        int x = generator.Next(5, 11);
-        int y = generator.Next(10, 21);
+        int punchDmg = generator.Next(5, 16);
+        int kickDmg = generator.Next(25, 51);
+
+
+        string answer = "";
+        answer = answer.ToLower();
+        while (answer != "punch" && answer != "kick" && answer != "slap")
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"{name1} punch or kick or slap?");
+            answer = Console.ReadLine();
+
+        }
+        if (answer == "punch")
+        {
+            int crit = generator.Next(100);
+            if (crit < 15)
+            {
+                player2 = player2 - 75;
+                Console.WriteLine($"{name1} did 75 damage");
+                Console.WriteLine($"{name2} has {player2}hp left");
+                Console.WriteLine();
+            }
+            else
+            {
+                player2 = player2 - punchDmg;
+                Console.WriteLine($"{name1} did {punchDmg} damage");
+                Console.WriteLine($"{name2} has {player2}hp left");
+                Console.WriteLine();
+            }
+        }
+        if (answer == "kick")
+        {
+            player2 = player2 - kickDmg;
+            Console.WriteLine($"{name1} did {kickDmg} damage");
+            Console.WriteLine($"{name2} has {player2}hp left");
+            Console.WriteLine();
+        }
+        if (answer == "slap")
+        {
+            int crit = generator.Next(100);
+            if (crit < 15)
+            {
+                player2 = player2 - 130;
+                Console.WriteLine($"{name1} did 130 damage");
+                Console.WriteLine($"{name2} has {player2}hp left");
+                Console.WriteLine();
+            }
+            else
+            {
+                player1 = player1 - 25;
+                Console.WriteLine($"{name2} did 25 damage to you");
+                Console.WriteLine($"{name1} has {player1}hp left");
+                Console.WriteLine();
+            }
+        }
+
+        if (player1 > 0 && player1 <= 10)
+        {
+            int instaKill = generator.Next(2);
+
+            Thread.Sleep(1000);
+            Console.WriteLine($"There is a 50% chance to kill instakill your opponent");
+            Thread.Sleep(2500);
+
+            if (instaKill == 0)
+            {
+                Console.WriteLine($"Lucky one you instakilled");
+                player1 = player1 - player1;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"Unlucky you lost");
+                Console.ReadLine();
+            }
+        }
+            if (player2 > 0 && player2 <= 10)
+        {
+            int instaKill = generator.Next(2);
+
+            Thread.Sleep(1000);
+            Console.WriteLine($"There is a 50% chance to kill instakill your opponent");
+            Thread.Sleep(2500);
+
+            if (instaKill == 0)
+            {
+                Console.WriteLine($"Lucky one you instakilled");
+                player2 = player2 - player2;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"Unlucky you lost");
+                Console.ReadLine();
+            }
+        }
+        return (player1, player2);
+    }
+
+
+    public static (int, int) fight2(int player2, int player1, string name1, string name2)
+    {
+        Random generator = new Random();
+        int punchDmg = generator.Next(5, 16);
+        int kickDmg = generator.Next(25, 51);
 
         string answer = "";
         answer = answer.ToLower();
 
-        while (answer != "punch" && answer != "kick")
+        while (answer != "punch" && answer != "kick" && answer != "slap")
         {
             Thread.Sleep(1000);
-            Console.WriteLine($"Player 2 punch or kick?");
+            Console.WriteLine($"{name2} punch or kick or slap?");
             answer = Console.ReadLine();
         }
         if (answer == "punch")
         {
-            P1hp = P1hp - x;
-            Console.WriteLine($"Player 1 has {P1hp}hp left");
+            int crit = generator.Next(100);
+
+            if (crit < 15)
+            {
+                player1 = player1 - 75;
+                Console.WriteLine($"{name2} did 75 damage");
+                Console.WriteLine($"{name1} has {player1}hp left");
+                Console.WriteLine();
+            }
+            else
+            {
+                player1 = player1 - punchDmg;
+                Console.WriteLine($"{name2} did {punchDmg} damage");
+                Console.WriteLine($"{name1} has {player1}hp left");
+                Console.WriteLine();
+            }
+
         }
         if (answer == "kick")
         {
-            P1hp = P1hp - y;
-            Console.WriteLine($"Player 1 has {P1hp}hp left");
+            player1 = player1 - kickDmg;
+            Console.WriteLine($"{name2} did {kickDmg} damage");
+            Console.WriteLine($"{name1} has {player1}hp left");
+            Console.WriteLine();
         }
+        if (answer == "slap")
+        {
+            int crit = generator.Next(100);
+
+            if (crit < 15)
+            {
+                player1 = player1 - 130;
+                Console.WriteLine($"{name2} did 130 damage");
+                Console.WriteLine($"{name1} has {player1}hp left");
+                Console.WriteLine();
+            }
+            else
+            {
+                player2 = player2 - 25;
+                Console.WriteLine($"{name1} did 25 damage to you");
+                Console.WriteLine($"{name2} has {player2}hp left");
+                Console.WriteLine();
+            }
+        }
+
+        if (player2 > 0 && player2 <= 10)
+        {
+            int instaKill = generator.Next(2);
+
+            Thread.Sleep(1000);
+            Console.WriteLine($"There is a 50% chance to kill instakill your opponent");
+            Thread.Sleep(2500);
+
+            if (instaKill == 0)
+            {
+                Console.WriteLine($"Lucky one you instakilled");
+                player2 = player2 - player2;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"Unlucky you lost");
+                Console.ReadLine();
+            }
+        }
+        if (player1 > 0 && player1 <= 10)
+        {
+            int instaKill = generator.Next(2);
+
+            Thread.Sleep(1000);
+            Console.WriteLine($"There is a 50% chance to kill instakill your opponent");
+            Thread.Sleep(2500);
+
+            if (instaKill == 0)
+            {
+                Console.WriteLine($"Lucky one you instakilled");
+                player1 = player1 - player1;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"Unlucky you lost");
+                Console.ReadLine();
+            }
+        }
+        return (player2, player1);
     }
 }
